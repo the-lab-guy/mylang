2be12970ab5b21b102481c706295c59c4b0f7384
popsharma <popsharma@users.noreply.github.com>
2024-07-11 17:05:16 +0100

    add very cmdline options to choose interpret or compile

--- mylang.py
+++ mylang.py
@@ -3,35 +3,45 @@ import os
 import mylangtools as mlt
 
 # read arguments
-program_filepath = sys.argv[1]
+user_action = sys.argv[1]
+program_filepath = sys.argv[2]
 print(f"source file: {program_filepath}")
 
-print("[CMD] Parsing")
-program, label_tracker = mlt.tokenise(program_filepath)
-print(program)
-print(label_tracker)
+if user_action == '-r':
+    # run program using interpreter
+    print("[CMD] Parsing")
+    program, label_tracker = mlt.tokenise(program_filepath)
+    print(program)
+    print(label_tracker)
 
-print("[CMD] Interpreting")
-status = mlt.interpret(program, label_tracker)
-print(status)
+    print("[CMD] Interpreting")
+    status = mlt.interpret(program, label_tracker)
+    print(status)
 
+elif user_action == '-c':
+    # compile program to executable
+    print("[CMD] Preprocessing")
+    program = mlt.ctokenise(program_filepath)
+    print(program)
 
-print("[CMD] Preprocessing")
-program = mlt.ctokenise(program_filepath)
-print(program)
+    string_literals = mlt.precompile(program)
+    print(string_literals)
 
-string_literals = mlt.precompile(program)
-print(string_literals)
+    print("[CMD] Compiling")
+    status, asm_filepath = mlt.compile(program_filepath, program, string_literals)
+    print(status, asm_filepath)
 
-print("[CMD] Compiling")
-status, asm_filepath = mlt.compile(program_filepath, program, string_literals)
-print(status, asm_filepath)
+    print("[CMD] Assembling")
+    os.system(f"nasm -g -f elf64 {asm_filepath}")
 
-print("[CMD] Assembling")
-os.system(f"nasm -g -f elf64 {asm_filepath}")
+    print("[CMD] Linking")
+    os.system(f"gcc -g -o {asm_filepath[:-4] + '.exe'} {asm_filepath[:-4] + '.o'}")
 
-print("[CMD] Linking")
-os.system(f"gcc -g -o {asm_filepath[:-4] + '.exe'} {asm_filepath[:-4] + '.o'}")
+    print(f"[INFO] Executable filename: {asm_filepath[:-4] + '.exe'}")
 
-print(f"[CMD] Running")
-os.system(f"{asm_filepath[:-4] + '.exe'}")
+else:
+    print("Invalid option:", user_action)
+    print("Options:")
+    print("\t-r <filename>  = run interpreted")
+    print("\t-c <filename>  = compile executable")
+
